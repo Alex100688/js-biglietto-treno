c2a3ac7a526ef9754cd90f50c45e1a40c3a55c1b
Alex100688 <alessiozingaro2@gmail.com>
2022-10-20 16:23:55 +0200

    Aggiunge lo sconro solo se inferiore ai 18 anni

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const discountYoung=Math.floor (ticket * 20 / 100);
 console.log (discountYoung);
 const discountSenior=Math.floor (ticket * 40 / 100);
 console.log (discountSenior);
-
+if(discountYoung < 18) {console.log("Sconto per te del 20%")};
 /*Il programma dovrà chiedere all'utente ilnumero di chilometri che vuole percorrere e l'età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
